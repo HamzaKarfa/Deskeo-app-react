ab1b21f512ed7e0b58e838323331840c15ee8c4e
HamzaKarfa <hamza.karfa@gmail.com>
2020-10-05 12:13:10 +0200

    commit 05/10/2020 fonctionnel

--- src/component/Admin/AdminImageFullScreenList/Modal.js
+++ src/component/Admin/AdminImageFullScreenList/Modal.js
@@ -32,7 +32,7 @@ const ModalConnect = ({updateStateContentCC2,props,updateStateContentCC3,state})
       if (button === '1') {
         updateStateContentCC2()
       } else {
-        if (state.ImageCarrouselContent3.length === 2) {
+        if (state.ImageCarrouselContent3.length >= 3) {
           alert('Vous ne pouvez pas mettre plus de 3 images sur cette slide')
         }else{
           updateStateContentCC3()
