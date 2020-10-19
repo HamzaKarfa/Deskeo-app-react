d8556a45b10338d7b1ca7953789d641f37f346a2
HamzaKarfa <hamza.karfa@gmail.com>
2020-10-19 14:10:27 +0200

    Modification du choix de l'administrateur + réorganisation fichier css

--- src/component/Admin/index.js
+++ src/component/Admin/index.js
@@ -70,46 +70,52 @@ function Admin() {
     }
     function displayTable(){
         return (
-            <table className={classes.center}>
-                <thead>
-                <tr>
-                    <th>Phrase du jour selectionné  
+            <div className='table'>
+                {displayResponseRequest()}
+                <table className={classes.center}>
+                    <thead>
+                    <tr>
+                        <th>Phrase du jour selectionné  
+                            <br/>     
+                            <DeleteIcon
+                                onClick={()=>{setPhraseDuJour('')}}
+                                type="delete"
+                                style={{height:'55px',width:'35px',color: "#CC160B" }}
+                                theme="outlined"
+                            />
+                        </th>
+                        <th>Image plein écran
+                            <br/>     
+                            <DeleteIcon
+                                onClick={()=>{setImageFullScreen('')}}
+                                type="delete"
+                                style={{height:'55px',width:'35px',color: "#CC160B" }}
+                                theme="outlined"
+                            />
+                        </th>
+                        <th>Multiple image plein écran
                         <br/>     
-                        <DeleteIcon
-                            onClick={()=>{setPhraseDuJour('')}}
-                            type="delete"
-                            style={{height:'55px',width:'35px',color: "#CC160B" }}
-                            theme="outlined"
-                        />
-                    </th>
-                    <th>Image plein écran
-                        <br/>     
-                        <DeleteIcon
-                            onClick={()=>{setImageFullScreen('')}}
-                            type="delete"
-                            style={{height:'55px',width:'35px',color: "#CC160B" }}
-                            theme="outlined"
-                        />
-                    </th>
-                    <th>Multiple image plein écran
-                    <br/>     
-                        <DeleteIcon
-                            onClick={()=>{resetMultipleImage()}}
-                            type="delete"
-                            style={{height:'55px',width:'35px',color: "#CC160B" }}
-                            theme="outlined"
-                        />
-                    </th>
-                </tr>
-                </thead>
-                <tbody >
-                <tr>
-                    <td className="contentTable">{phraseDuJour.phrases_of_day}<br/><img src={phraseDuJour.image} className={classes.imageTable} alt=''/></td>
-                    <td className="contentTable"><img src={imageFullScreen.images_path} className={classes.imageTable} alt=''/></td>
-                    <td className="contentTable">{displayElementTable()}</td>
-                </tr>
-                </tbody>
-            </table>
+                            <DeleteIcon
+                                onClick={()=>{resetMultipleImage()}}
+                                type="delete"
+                                style={{height:'55px',width:'35px',color: "#CC160B" }}
+                                theme="outlined"
+                            />
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody >
+                    <tr>
+                        <td className="contentTable">{phraseDuJour.phrases_of_day}<br/><img src={phraseDuJour.image} className={classes.imageTable} alt=''/></td>
+                        <td className="contentTable"><img src={imageFullScreen.images_path} className={classes.imageTable} alt=''/></td>
+                        <td className="contentTable">{displayElementTable()}</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <Button className='buttonSend' onClick={(()=>sendAdminChoice())}>
+                    Valider les Modifications
+                </Button>
+            </div>
         )
     }
     return (
@@ -124,17 +130,9 @@ function Admin() {
                     </Tabs>
                 </div>
             </AppBar>
-            <div className='table'>
-                {displayResponseRequest()}
-                
-                <Button className='buttonSend' onClick={(()=>sendAdminChoice())}>
-                    Valider les Modifications
-                </Button>
-            </div>
 
             <TabPanel value={value} index="one">
-                {displayTable()}
-                
+                {displayTable()}         
                 <ContentTab Phrase={setPhraseDuJour} 
                             Request={setResponseRequest}
                             varRequest={responseRequest}
